342ddcb7bbdb4b5816978708a88b4439336f9640
ntBre <brentrwestbrook@gmail.com>
2019-07-27 11:35:35 -0500

    Initial commit

---
+++ init.el
@@ -0,0 +1,26 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (leuven)))
+ '(evil-search-module (quote evil-search))
+ '(helm-mode t)
+ '(line-number-mode nil)
+ '(package-selected-packages (quote (powerline helm evil-visual-mark-mode)))
+ '(projectile-mode t nil (projectile)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(org-babel-load-file "~/.emacs.d/configuration.org")
